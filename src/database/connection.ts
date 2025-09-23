5cd22954fb9e15fbca86002d3a109896228ccdf6
Pedro Matheus <pedromatheus.ch@gmail.com>
2025-09-23 02:13:37 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -14,6 +14,7 @@ const connectionOptions: mysql.PoolOptions = {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  charset: 'utf8mb4',
   ssl: {
     rejectUnauthorized: true,
   }

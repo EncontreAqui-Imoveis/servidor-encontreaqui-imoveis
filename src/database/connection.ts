6112676bef1a5dda825f96be076afb5d6e45fbd6
Pedro Matheus <pedromatheus.ch@gmail.com>
2025-09-22 23:19:52 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,16 +1,22 @@
-import mysql from 'mysql2/promise';
-import 'dotenv/config';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true, 
-  connectionLimit: 10,       
-  queueLimit: 0             
-});
-
-console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER, // Lê o User do Railway
+  password: process.env.DATABASE_PASSWORD, // Lê a Password do Railway
+  database: process.env.DATABASE_NAME, // Lê o nome da Database do Railway
+  port: 4000, 
+  ssl: {
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}).promise();
 
 export default connection;

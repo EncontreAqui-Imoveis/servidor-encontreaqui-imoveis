414191daad420fbfbb369fdf5b56a70a8f7fca2a
Ctrshift <pedromatheus.ch@gmail.com>
2026-02-12 19:13:31 -0300

    feat: Divisão do Firebase SDK para não ocorrer aviso deprecated do punycode.

--- dist/config/firebaseAdmin.js
+++ dist/config/firebaseAdmin.js
@@ -1,43 +1,12 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const admin = __importStar(require("firebase-admin"));
 const fs_1 = __importDefault(require("fs"));
+const app_1 = require("firebase-admin/app");
+const auth_1 = require("firebase-admin/auth");
+const messaging_1 = require("firebase-admin/messaging");
 function loadServiceAccountFromEnv() {
     const requiredEnvVars = [
         'FIREBASE_PROJECT_ID',
@@ -74,8 +43,18 @@ if (serviceAccountPath) {
 else {
     serviceAccount = loadServiceAccountFromEnv();
 }
-// Inicializar o Firebase Admin
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-exports.default = admin;
+const app = (0, app_1.getApps)().length > 0
+    ? (0, app_1.getApps)()[0]
+    : (0, app_1.initializeApp)({
+        credential: (0, app_1.cert)(serviceAccount),
+    });
+const firebaseAdmin = {
+    app,
+    auth() {
+        return (0, auth_1.getAuth)(app);
+    },
+    messaging() {
+        return (0, messaging_1.getMessaging)(app);
+    },
+};
+exports.default = firebaseAdmin;

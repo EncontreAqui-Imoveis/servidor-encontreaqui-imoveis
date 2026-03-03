97bd1bccf3bb5b36087890c3c55f8c50de1ff655
PedroM <pmgamer790@gmail.com>
2026-03-03 18:18:41 -0300

    refactor(backend): endurece arquitetura, cobertura, observabilidade e testes de contrato. Removido dists.

--- dist/routes/public.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const PropertyController_1 = require("../controllers/PropertyController");
-const publicRoutes = (0, express_1.Router)();
-publicRoutes.get('/properties', PropertyController_1.propertyController.listPublicProperties);
-publicRoutes.get('/public/properties/:id', PropertyController_1.propertyController.showPublic);
-publicRoutes.get('/properties/cities', PropertyController_1.propertyController.getAvailableCities);
-exports.default = publicRoutes;
